32b8b11ef6db9ba6dcb019ef3706dc4c7e66a15d
ejjer <belkoegor1@gmail.com>
2022-04-20 22:46:33 +0300

    les2

--- app/src/main/java/com/example/note/ui/NoteCalendarFragment.java
+++ app/src/main/java/com/example/note/ui/NoteCalendarFragment.java
@@ -9,11 +9,28 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.note.R;
+import com.example.note.domain.Note;
+
+import java.util.Calendar;
+
 
 public class NoteCalendarFragment extends Fragment {
+
+    private static final String ARG_CALENDAR = "ARG_CALENDAR";
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        public static NoteCalendarFragment newInstance(Calendar calendar) {
+
+            Bundle args = new Bundle();
+            args.putParcelable(ARG_CALENDAR, calendar);
+
+            NoteCalendarFragment fragment = new NoteCalendarFragment();
+            fragment.setArguments(args);
+            return fragment;
+        }
+
 
         Toolbar toolbar = view.findViewById(R.id.toolbar);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -23,5 +40,7 @@ public class NoteCalendarFragment extends Fragment {
                         .popBackStack();
             }
         });
+
+
     }
 }
